013f1a2518b111ded78eaa5e27237d9c765ecf8d
Ahmed K. A <ahmadkabdullah@pm.me>
2022-11-04 21:42:33 +0300

    init: add to git

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import { useCounterStore } from "./stores/counter";
+
+  const counterStore = useCounterStore();
+</script>
+
+<template>
+  <h1>{{ counterStore.count }}</h1>
+  <button @click="counterStore.increment"></button>
+</template>
---
+++ src/stores/counter.ts
@@ -0,0 +1,11 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+
+export const useCounterStore = defineStore("counter", () => {
+  const count = ref(0);
+  const increment = () => {
+    count.value++;
+  };
+
+  return { count, increment };
+});
