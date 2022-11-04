57e4e2f7280a720a913fc3b4f99b19d3256dbd18
Ahmed K. A <ahmadkabdullah@pm.me>
2022-11-04 22:23:52 +0300

    feat: add naive-ui and vicons

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <script setup lang="ts">
   import { useMainStore } from "./store";
+  import { PlusFilled } from "@vicons/material";
+  import {
+    NGradientText,
+    NIcon,
+    NButton,
+    NGlobalStyle,
+    NConfigProvider,
+    darkTheme,
+  } from "naive-ui";
 
   const store = useMainStore();
 </script>
 
 <template>
-  <h1>{{ store.count }}</h1>
-  <button @click="store.increment">Add</button>
+  <NConfigProvider :theme="darkTheme">
+    <!-- app -->
+    <div class="app">
+      <NGradientText :size="40">
+        {{ store.count }}
+      </NGradientText>
+      <br />
+      <NButton @click="store.increment">
+        <NIcon size="40" :component="PlusFilled"></NIcon>
+      </NButton>
+    </div>
+
+    <!-- style to body -->
+    <NGlobalStyle></NGlobalStyle>
+  </NConfigProvider>
 </template>
+
+<style scoped></style>
