488e2508d0ee99acb14c36cf4acd1c1e4b22cd82
Ahmed K. A <ahmadkabdullah@pm.me>
2022-11-13 15:06:50 +0300

    refactor: change indentation to tabs

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,45 @@
 <script setup lang="ts">
-  import { useMainStore } from "./store";
-  import { PlusFilled } from "@vicons/material";
-  import {
-    NGradientText,
-    NCard,
-    NIcon,
-    NButton,
-    NGlobalStyle,
-    NConfigProvider,
-    darkTheme,
-  } from "naive-ui";
+	import { useMainStore } from "./store";
+	import { PlusFilled } from "@vicons/material";
+	import {
+		NGrid,
+		NGridItem,
+		NGradientText,
+		NCard,
+		NIcon,
+		NButton,
+		NGlobalStyle,
+		NConfigProvider,
+		darkTheme,
+	} from "naive-ui";
 
-  const store = useMainStore();
+	const store = useMainStore();
 </script>
 
 <template>
-  <NConfigProvider :theme="darkTheme">
-    <!-- app -->
-    <NCard title="App">
-      <NGradientText :size="40">
-        {{ store.count }}
-      </NGradientText>
-      <br />
-      <NButton @click="store.increment">
-        <NIcon size="40" :component="PlusFilled"></NIcon>
-      </NButton>
-    </NCard>
+	<NConfigProvider :theme="darkTheme">
+		<NGrid>
+			<NGridItem>
+				<NCard>
+					<NGradientText :size="40">
+						{{ store.count }}
+					</NGradientText>
+					<br />
+					<NButton @click="store.increment">
+						<NIcon size="40" :component="PlusFilled"></NIcon>
+					</NButton>
+				</NCard>
+			</NGridItem>
 
-    <!-- style to body -->
-    <NGlobalStyle></NGlobalStyle>
-  </NConfigProvider>
+			<NGridItem>
+				<NCard>
+					<NGradientText :size="40">
+						{{ store.count }}
+					</NGradientText>
+				</NCard>
+			</NGridItem>
+		</NGrid>
+		<!-- style to body -->
+		<NGlobalStyle></NGlobalStyle>
+	</NConfigProvider>
 </template>
-
-<style scoped></style>
--- src/store.ts
+++ src/store.ts
@@ -2,10 +2,10 @@ import { ref } from "vue";
 import { defineStore } from "pinia";
 
 export const useMainStore = defineStore("main", () => {
-  const count = ref(0);
-  const increment = () => {
-    count.value++;
-  };
+	const count = ref(0);
+	const increment = () => {
+		count.value++;
+	};
 
-  return { count, increment };
+	return { count, increment };
 });
