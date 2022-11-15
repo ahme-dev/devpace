eb1893fd642be2f791bba468a82121ed36235c8a
Ahmed K. A <ahmadkabdullah@pm.me>
2022-11-15 15:23:06 +0300

    feat: add structure of pomodoro component

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -1,12 +1,48 @@
 <script setup lang="ts">
-	import { NCard, NText, NGradientText, NSpace } from "naive-ui";
+	import { PlayArrowFilled } from "@vicons/material";
+	import {
+		NCard,
+		NText,
+		NSpace,
+		NSlider,
+		NGrid,
+		NGridItem,
+		NTimeline,
+		NTimelineItem,
+		NButton,
+		NIcon,
+	} from "naive-ui";
 </script>
 
 <template>
 	<NCard>
-		<NSpace vertical>
-			<NText type="primary">Do</NText>
-			<NText type="primary">Do</NText>
-		</NSpace>
+		<NGrid :cols="2">
+			<NGridItem>
+				<NSpace vertical :size="40">
+					<NText size="large" type="primary">do</NText>
+					<NSlider></NSlider>
+					<NSlider></NSlider>
+					<NSlider></NSlider>
+					<NSlider></NSlider>
+
+					<NText> Something about your session </NText>
+
+					<NButton style="margin-top: 1rem">
+						<template #icon>
+							<NIcon> <PlayArrowFilled></PlayArrowFilled> </NIcon>
+						</template>
+						Start
+					</NButton>
+				</NSpace>
+			</NGridItem>
+			<NGridItem>
+				<NSpace justify="center" style="overflow: auto">
+					<NTimeline>
+						<NTimelineItem content="Something" time="22:00" />
+						<NTimelineItem content="Something" time="22:00" />
+					</NTimeline>
+				</NSpace>
+			</NGridItem>
+		</NGrid>
 	</NCard>
 </template>
