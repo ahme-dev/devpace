b8a4526e30e70c5e635cf53e9f87d6da5fd95bf6
Ahmed K. A <ahmadkabdullah@pm.me>
2022-11-30 19:17:15 +0300

    feat: add var for api text

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,12 +1,15 @@
 <script lang="ts" setup>
 	import { NButton, NGradientText, NIcon } from "naive-ui";
 	import { ArrowCircleRightRound, AccessibilityFilled } from "@vicons/material";
+	import { ref } from "vue";
+
+	let text = ref("Take your time, you'll make something great.");
 </script>
 
 <template>
 	<div style="display: flex; flex-direction: column">
 		<!-- top -->
-		<div>Take your time, you'll make something great.</div>
+		<div>{{ text }}</div>
 
 		<!-- bottom -->
 		<div style="display: flex; gap: 1rem; align-items: center">
