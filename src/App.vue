a619f00d6001968a1c1bd40a425583c6ce2d56b4
Ahmed K.A <ahmadkabdullah@pm.me>
2022-12-17 21:25:58 +0300

    style: change to roboto font

--- src/App.vue
+++ src/App.vue
@@ -31,10 +31,10 @@
 </template>
 
 <style>
-	@import url("https://fonts.googleapis.com/css2?family=Azeret+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+	@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap");
 
 	.app {
-		font-family: "Azeret Mono", monospace;
+		font-family: "Roboto", sans-serif;
 
 		min-height: 100vh;
 		min-width: 100vw;
